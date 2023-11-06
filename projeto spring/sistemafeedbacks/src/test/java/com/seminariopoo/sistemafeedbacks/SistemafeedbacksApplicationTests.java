652412567f11987acddc9087a359c77082896a05
Kauan_kelv <126915091+Kauankelv@users.noreply.github.com>
2023-11-06 01:47:32 -0300

    Add files via upload

---
+++ projeto spring/sistemafeedbacks/src/test/java/com/seminariopoo/sistemafeedbacks/SistemafeedbacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seminariopoo.sistemafeedbacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemafeedbacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
